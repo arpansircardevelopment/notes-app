89514320be70ba3bb4b736b75899742783e7e575
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2022-10-11 08:54:29 +0530

    Account page layout - Design Complete

--- app/src/main/java/com/arpansircar/notes_app/presentation/fragment/AccountFragment.kt
+++ app/src/main/java/com/arpansircar/notes_app/presentation/fragment/AccountFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import coil.load
+import com.arpansircar.notes_app.R
 import com.arpansircar.notes_app.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
@@ -17,6 +19,12 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAccountBinding.inflate(inflater, container, false)
+        binding?.apply {
+            avProfileImage.avatarInitials = "A"
+            tvUserName.text = "Arpan Sircar"
+            tvUserEmail.text = "arpansircar1@gmail.com"
+        }
+
         return binding?.root
     }
 }
